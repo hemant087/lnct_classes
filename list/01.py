ac3b565abc54fb8db2350338c311f888e1c73b7c
hemant087 <88886834+hemant087@users.noreply.github.com>
2024-11-25 15:15:57 +0530

    .

--- list/01.py
+++ list/01.py
@@ -1,11 +1,67 @@
-D = {}
+# num = (1+2j)
 
-D["Name"] = "Hemant Raj"
-D["Roll"] = 587
+# print(type(num))
 
-for i in D:
-    print(f"{i}---->{D[i]}")
+# st = "Hi my name is "
+
+# for i in st:
+#     print(i)
+
+# a= {1,2,3,4}
+# b = {5,1,7,8}
+# d = a&b
+# print(d)
+
+
+# D = {}
+
+# for i in range(5):
+#     key = input("Enter the key: ")
+#     temp = []
+#     for i in range(4):
+#         Val = input("Enter the value: ")
+#         temp.append(Val)
+#     D[key] = Val 
+# print(D)
+
+# print({True:"Hemat",False:"5667"})
+# print([1,2,3,4]:[5,6,7,8])
 
 
 
-print(D)
+# there is shop i need to buy it,, first check that "A" is available or not,,,
+# if Yes: should i buy it or not (Y/N)
+# Yes: Ask price 
+# print ==> i bought "A"
+
+# a = input("Hello ! will i get A-Product in you shop (Y/N):  ")
+# amount = 100
+
+# if a == "Y":
+#     print("Yes, its available ")
+#     price = int(input(" What will be the price of it:  "))
+#     if price <= amount:
+#         print("i bought")
+# else:
+#     print("Sorry A-product is not available")
+
+# if a=="Y" and int(input(" What will be the price of it:  ")) <= amount:
+#     print()
+
+
+num = (1,2,3,4)
+num2 = (5,6,7,8)
+# (6,8,10,12)
+ans = ()
+temp = (1)
+print(f"{temp}----> {type(temp)}")
+temp2 = (1,)
+print(f"{temp2}----> {type(temp2)}")
+
+
+for i in range(4):
+    ans = ans + (num[i]+num2[i],)
+
+print(ans)
+
+# immutable --> cant change the value
