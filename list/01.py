a9fa291c40c223ade7ff72a951771a8b77e46e27
hemant087 <88886834+hemant087@users.noreply.github.com>
2024-11-26 16:09:35 +0530

    .

--- list/01.py
+++ list/01.py
@@ -49,19 +49,79 @@
 #     print()
 
 
-num = (1,2,3,4)
-num2 = (5,6,7,8)
-# (6,8,10,12)
-ans = ()
-temp = (1)
-print(f"{temp}----> {type(temp)}")
-temp2 = (1,)
-print(f"{temp2}----> {type(temp2)}")
+# num = (1,2,3,4)
+# num2 = (5,6,7,8)
+# # (6,8,10,12)
+# ans = ()
+# temp = (1)
+# print(f"{temp}----> {type(temp)}")
+# temp2 = (1,)
+# print(f"{temp2}----> {type(temp2)}")
 
 
-for i in range(4):
-    ans = ans + (num[i]+num2[i],)
+# for i in range(4):
+#     ans = ans + (num[i]+num2[i],)
 
-print(ans)
+# print(ans)
 
 # immutable --> cant change the value
+
+
+# make a list where you need to append the 3 students details(name,roll,class,phone no)
+
+# lst = []
+
+
+# condition_1 = True
+# condition_2 = True
+
+# # code -1
+# if condition_1:
+#     print("condition 1 is True")
+#     if condition_2:
+#         pass
+
+
+# # code -2 
+# if condition_1 and condition_2:
+#         pass
+
+
+# for i in range(10):
+#     pass
+
+
+t = (3,2,7,10)
+
+# num = int(input("enter the number: "))
+
+# print(t + ((t[t.index(10)])+num,))
+
+
+# for i in t:
+#     print(i)
+
+# temp = list(t)    # tuples converted in list
+# num = temp.index(max(temp))  
+# temp[num] = temp[num] + int(input("enter the number"))
+# t = tuple(temp)
+
+# print(t)
+# num1 = temp.index(max(t))  
+# print(t[num1])
+
+
+d = {}  # dictionary declare
+n = 3
+for i in range(n): # loop for the row iteration
+    print(i,n)
+    temp = []   # temporary list for the column entry
+    k = input("Enter the name of Key :  ")
+    if k not in d:
+        for j in range(4): #loop for the column iteration
+            temp.append(input("Enter the value: ")) 
+        d[k] = temp  # assigning value(list) in key
+    else:
+        print("This key is already taken")
+        continue
+print(d)
