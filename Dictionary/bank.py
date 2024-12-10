0024d72f6e662657fa538507961061bda3772696
hemant087 <88886834+hemant087@users.noreply.github.com>
2024-12-10 15:50:59 +0530

    Update bank.py

--- Dictionary/bank.py
+++ Dictionary/bank.py
@@ -1,3 +1,23 @@
+bank = {}
+# function for filling details in the bank
+
+
+def details(bank, user_name):
+    det = {}
+    det["password"] = input(f"{user_name} Enter your password: ")
+    det["name"] = input(f"{user_name} Enter your Name: ")
+    det["address"] = input(f"{user_name} Enter your Address: ")
+    det["id"] = input(f"{user_name} Enter your ID Number: ")
+    det["contact"] = input(f"{user_name} Enter your Contact Number: ")
+    bank[user_name] = det
+    return bank
+# function for updating the details
+
+
+def update(bank):
+    status = login(bank)
+    if status == True:
+        bank = details(bank, bank[user_name])
 
 
 # function for login
@@ -5,36 +25,56 @@ def login(bank):
     user_name = input("Enter the user name:  ")
     if user_name in bank:
         password = input(f"{user_name} Enter your password: ")
-        if password == bank[user_name]:
-            print(f"{user_name} Welcome to the bank, how can i help you sir/ma'am ")
+        if password == bank[user_name][password]:
+            print(f"{user_name} Welcome to the bank, how can I help you, sir/ma'am?")
+            return True
         else:
-            print(f"oops! wrong password, {user_name}")
+            print(f"Oops! Wrong password, {user_name}.")
     else:
-        print("User not found")
+        print("User not found.")
     return bank
 
-# functions for creating accounts
+# function for creating accounts
+
+
 def create_account(bank):
     user_name = input("Create user name: ")
     if user_name not in bank:
-        password = input(f"{user_name} create password: ")
-        bank[user_name] = password
-        print(f"{user_name} your account created")
+        bank = details(bank, user_name)
+        print(f"{user_name}, your account is created.")
     else:
-        print("user name is already taken")
+        print("User name is already taken.")
     return bank
 
-# your main 
-bank = {}
-print(''' Welcome to --:Appna Bank:--
-            How may i help you sir/ma'am:
-            press 1: for login to your account
-            press 2: for creating new account
 
-''')
-key_pressed = int(input("Press the key: "))
-if key_pressed == 1:
-    login()
-elif key_pressed == 2:
-    create_account()
-    print(bank)
+# main
+while True:
+    print(''' 
+    Welcome to --:Appna Bank:--
+    How may I help you, sir/ma'am:
+    Press 1: for login to your account
+    Press 2: for creating a new account
+    Press 3: for updating your details
+    Press 4: for Exit the bank
+
+    ''')
+
+    key_pressed = int(input("Press the key: "))
+    if key_pressed == 1:
+        login(bank)  # Pass the bank dictionary
+    elif key_pressed == 2:
+        create_account(bank)  # Pass the bank dictionary
+        print(bank)
+    elif key_pressed == 3:
+        user_name = input("Enter the user name:  ")
+        if user_name in bank:
+            password = input(f"{user_name} Enter your password: ")
+            if password == bank[user_name][password]:
+                print(f"{user_name}, your account ready for update")
+                update(bank,user_name)
+            else:
+                print(f"Oops! Wrong password, {user_name}.")
+        else:
+            print("User not found.")
+    elif key_pressed == 4:
+        break
